ff5e0465a733fc55fa13642665d303417e30f0d0
Tomson331 <133116477+Tomson331@users.noreply.github.com>
2023-07-07 12:16:03 +0200

    String Tabulacja, Nowa linia, pusta linia, print wiele argumentów, String podzielony na wiele linii kodu, wydrukonanie znaku bez interpretacji kodu.

---
+++ main.py
@@ -0,0 +1,20 @@
+# Day 12 - String 5 cdn
+
+print("Zero\nTo\n Junior")
+
+print() # pusta linia
+
+print("Zero\nTo\n Junior\t i tabulacja")
+
+
+
+print("\"Zero\" to \"Junior\"")
+
+print("Zero","to","Junior",10000)
+
+print('''
+Hej
+co
+tam
+w Zero to Junior?
+''')
